b486995a136e5367c6b61e088f7010fcfe108d73
Andrew Lampert <alampert@contractorcompliance.io>
2022-03-17 09:46:46 -0400

    init login page

---
+++ src/components/auth/LoginCC.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Login</h1>
+        <v-input />
+    </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style></style>
---
+++ src/views/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <h1>Login Page</h1>
+      <LoginCC />
+  </div>
+</template>
+
+<script>
+import LoginCC from "@/components/auth/LoginCC.vue";
+
+export default {
+    components:{
+        LoginCC
+    }
+}
+</script>
+
+<style>
+
+</style>
